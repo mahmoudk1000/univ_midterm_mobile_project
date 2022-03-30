8404b9aaf12e735040a9d05f9dd80378bf05a3e0
mahmoudk1000 <mahmoudk1000@gmail.com>
2022-03-30 19:55:56 +0200

    Made Random App

--- app/src/main/java/com/example/vierapps/RandomActivity.java
+++ app/src/main/java/com/example/vierapps/RandomActivity.java
@@ -1,12 +1,34 @@
 package com.example.vierapps;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 public class RandomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.random_activity);
+    }
+
+    public void random_create(View view) {
+        EditText min_num = (EditText) findViewById(R.id.min_num);
+        String min = min_num.getText().toString();
+        int int_min = Integer.parseInt(min);
+
+        EditText max_num = (EditText) findViewById(R.id.max_num);
+        String max = max_num.getText().toString();
+        int int_max = Integer.parseInt(max);
+
+        Random number = new Random();
+        int random_num = number.nextInt(int_max + 1 - int_min) + int_min;
+
+        TextView counter = (TextView) findViewById(R.id.show_random);
+        counter.setText(String.valueOf(random_num));
     }
 }
