3d82f0ec3a78154cfeafdae67ec7f2948283b2a8
mahmoudk1000 <mahmoudk1000@gmail.com>
2022-03-31 19:19:51 +0200

    Date and Stopwatch comming soon

--- app/src/main/java/com/example/vierapps/StopActivity.java
+++ app/src/main/java/com/example/vierapps/StopActivity.java
@@ -1,12 +1,24 @@
 package com.example.vierapps;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StopActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stop_activity);
+    }
+    public void countDown(View view) {
+        EditText counter_down = (EditText) findViewById(R.id.show_count_down);
+        String count_down = counter_down.getText().toString();
+        int count_down_value = Integer.parseInt(count_down);
+        while( count_down_value > 0 ){
+            count_down_value--;
+            counter_down.setText(String.valueOf(count_down_value));
+        }
     }
 }
