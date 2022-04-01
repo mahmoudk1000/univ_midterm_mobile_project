d84e73b9bae75f6f0744dc597bc4d67f0a54bf80
mahmoudk1000 <mahmoudk1000@gmail.com>
2022-04-02 00:36:46 +0200

    Get ready for 2.0

--- app/src/main/java/com/example/vierapps/DateActivity.java
+++ app/src/main/java/com/example/vierapps/DateActivity.java
@@ -19,28 +19,28 @@ public class DateActivity extends AppCompatActivity {
     public void birthday(View view) {
         int year = Calendar.getInstance().get(Calendar.YEAR);
 
-        EditText input_year = (EditText) findViewById(R.id.year_date);
+        EditText input_year = findViewById(R.id.year_date);
         String year_value = input_year.getText().toString();
         int final_year_value = Integer.parseInt(year_value);
 
-        EditText input_month = (EditText) findViewById(R.id.month_date);
+        EditText input_month = findViewById(R.id.month_date);
         String month_value = input_month.getText().toString();
         int final_month_value = Integer.parseInt(month_value);
 
-        EditText input_day = (EditText) findViewById(R.id.day_date);
+        EditText input_day = findViewById(R.id.day_date);
         String day_value = input_day.getText().toString();
         int final_day_value = Integer.parseInt(day_value);
 
-        TextView show_year = (TextView) findViewById(R.id.show_year);
+        TextView show_year = findViewById(R.id.show_year);
         int years_num = year - final_year_value;
-        show_year.setText(String.valueOf(years_num));
+        show_year.setText(String.valueOf(years_num + "Y,"));
 
-        TextView show_month = (TextView) findViewById(R.id.show_month);
+        TextView show_month = findViewById(R.id.show_month);
         int months_num = years_num  * 12;
-        show_month.setText(String.valueOf(months_num));
+        show_month.setText(String.valueOf(months_num + "M,"));
 
-        TextView show_day = (TextView) findViewById(R.id.show_day);
-        int days_num = months_num * 30;
-        show_day.setText(String.valueOf(days_num));
+        TextView show_day = findViewById(R.id.show_day);
+        int days_num = years_num * 365;
+        show_day.setText(String.valueOf(days_num + "D"));
     }
 }
--- app/src/main/java/com/example/vierapps/StopActivity.java
+++ app/src/main/java/com/example/vierapps/StopActivity.java
@@ -11,21 +11,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.concurrent.TimeUnit;
 
 public class StopActivity extends AppCompatActivity {
+    public CountDownTimer meinCountDownTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stop_activity);
     }
 
-    public void countDown(View view) {
-        EditText count_down_value = (EditText) findViewById(R.id.count_down_value);
+    public void countDownTimerFun(boolean decide) {
+        EditText count_down_value = findViewById(R.id.count_down_value);
         String count_down_in_minutes = count_down_value.getText().toString();
         int final_count_down_in_minutes = Integer.parseInt(count_down_in_minutes);
+        TextView show_count_down = findViewById(R.id.show_count_down);
 
-        TextView show_count_down = (TextView) findViewById(R.id.show_count_down);
-        int milliseconds = final_count_down_in_minutes * 60 * 1000;
+        int milliseconds;
+        if (decide){
+            milliseconds = final_count_down_in_minutes * 60 * 1000;
+        }else{
+            milliseconds = 0;
+        }
 
-        CountDownTimer countDownTimer = new CountDownTimer(milliseconds, 1000) {
+        meinCountDownTimer = new CountDownTimer(milliseconds, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 show_count_down.setText(String.format("%02d:%02d",
@@ -38,9 +45,20 @@ public class StopActivity extends AppCompatActivity {
                 show_count_down.setText(R.string.time_up);
             }
         };
-        if(view.getId() == R.id.count_down)
-            countDownTimer.start();
-        else if(view.getId() == R.id.count_down)
-            countDownTimer.cancel();
+    }
+    public void start_countDown(View view) {
+        if(view.getId() == R.id.start_count_down) {
+            countDownTimerFun(true);
+            meinCountDownTimer.start();
+        }
+    }
+
+    public void stop_countDown(View view) {
+        if(view.getId() == R.id.stop_count_down) {
+            TextView show_count_down = findViewById(R.id.show_count_down);
+            meinCountDownTimer.cancel();
+            countDownTimerFun(false);
+            show_count_down.setText("00:00");
+        }
     }
 }
