40bf0690a88ce5cf7aa3fd08b4b29a75b0295a3a
mahmoudk1000 <mahmoudk1000@gmail.com>
2022-04-04 12:08:06 +0200

    Now Date app does really work well

--- app/src/main/java/com/example/vierapps/DateActivity.java
+++ app/src/main/java/com/example/vierapps/DateActivity.java
@@ -7,7 +7,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class DateActivity extends AppCompatActivity {
     @Override
@@ -17,8 +18,6 @@ public class DateActivity extends AppCompatActivity {
     }
 
     public void birthday(View view) {
-        int year = Calendar.getInstance().get(Calendar.YEAR);
-
         EditText input_year = findViewById(R.id.year_date);
         String year_value = input_year.getText().toString();
         int final_year_value = Integer.parseInt(year_value);
@@ -31,16 +30,17 @@ public class DateActivity extends AppCompatActivity {
         String day_value = input_day.getText().toString();
         int final_day_value = Integer.parseInt(day_value);
 
+        LocalDate birthday = LocalDate.of(final_year_value, final_month_value, final_day_value);
+        LocalDate now = LocalDate.now();
+        Period diff = Period.between(birthday, now);
+
         TextView show_year = findViewById(R.id.show_year);
-        int years_num = year - final_year_value;
-        show_year.setText(String.valueOf(years_num + "Y,"));
+        show_year.setText(String.valueOf(diff.getYears() + "Y,"));
 
         TextView show_month = findViewById(R.id.show_month);
-        int months_num = years_num  * 12;
-        show_month.setText(String.valueOf(months_num + "M,"));
+        show_month.setText(String.valueOf(diff.getMonths() + "M,"));
 
         TextView show_day = findViewById(R.id.show_day);
-        int days_num = years_num * 365;
-        show_day.setText(String.valueOf(days_num + "D"));
+        show_day.setText(String.valueOf(diff.getDays() + "D"));
     }
 }
